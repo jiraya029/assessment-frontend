4cd945cc907940095c732ff4ac50d335e5bd01f3
Darwin Francis <you@example.com>
2026-07-21 23:24:17 +0530

    FIX APIi

--- vite.config.ts
+++ vite.config.ts
@@ -8,11 +8,6 @@ export default defineConfig({
   server: {
     host: "0.0.0.0",
     port: 5173,
-    proxy: {
-      "https://assessment-backend-production-a284.up.railway.app": {
-        target: "http://127.0.0.1:4000",
-        changeOrigin: true,
-      },
-    },
+    
   },
 })
